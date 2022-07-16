323a80214dfe0a68f950e5b5eccb92a816d567cc
memelopez <meme30lopez@gmail.com>
2022-07-16 00:41:48 -0600

    Add quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div className="quote-div">
+      <span className="actual-quote">
+        Mathematics is not about numbers, equations, computations, or algorithhms:
+        it is about understanding. -William Paul Thurston
+      </span>
+    </div>
+  );
+}
+
+export default Quote;
